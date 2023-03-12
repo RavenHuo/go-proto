d86991b7b53b8853a9a311cbf831a68a79cef159
huoruiwen <ruiwen.rw@nonolive.com>
2023-03-12 12:27:28 +0800

    proto ->pb

--- stock-proto/make.sh
+++ stock-proto/make.sh
@@ -1,23 +1,40 @@
 #!/usr/bin/env bash
+cd $(dirname $0)
 
-protos_dir="./protos"
-domin="github.com"
-org="RavenHuo"
-# 这里是路径
-project_name="go-proto/stock-proto"
+protoDir="protos"
 pb_package="pkg"
 
-## batch clean old code
-# gsed -i "s/^\(package\) \(.*;\)/\1 nonoapi;/" protos/**/*.proto
-sed -i "/option go_package.*/d" protos/**/*.proto
-files=$(ls -l ${protos_dir} |awk '/^d/ {print $NF}')
+PROTOC=protoc
+if (which protoc.exe) then
+    PROTOC=protoc.exe
+fi
 
 
-echo "==============================================="
-echo "Compile"
-echo "==============================================="
-for dirname in $files
-do
-  echo "change compile package option => ${domin}/${org}/${project_name}/${pb_package}/${dirname}"
-  res=$(sed -i "s?^\(package .*;\)?\1\\noption go_package = \"${domin}\\/${org}\\/${project_name}\\/${pb_package}\\/${dirname}\";?" ${protos_dir}/${dirname}/*.proto)
+if [ -e ${pb_package} ]
+then
+echo "${pb_package} 已存在"
+else
+mkdir ${pb_package}
+echo "${pb_package}目录不存在，创建文件夹"
+fi
+
+
+version=`$PROTOC --version`
+
+cd ${protoDir}
+
+for proto_file in */*.proto; do
+    ## 替换路径
+    pb_file=${proto_file/protos/pkg}
+    pb_file=${pb_file/.proto/.pb.go}
+    echo $proto_file
+    echo $pb_file
+    if [ ! -e $pb_file ] || [ $proto_file -nt $pb_file ]; then
+
+        echo "Compiling $proto_file -> $pb_file ..."
+        # 输出到 ../${pb_package}
+        # 一般都会使用source_relative,否则，就会按照option go_package里面的那个路径生成
+        # -I 表示包含全部
+        $PROTOC  -I $proto_file --plugin=protoc-gen-go=../../bin/protoc-gen-go.exe --go_out=../${pb_package} --go_opt=paths=source_relative
+    fi
 done
