8791e6a51acb7674305e4ba2f867739907c92173
huoruiwen <ruiwen.rw@nonolive.com>
2023-03-12 15:35:56 +0800

    pb

--- stock-proto/make.sh
+++ stock-proto/make.sh
@@ -22,19 +22,21 @@ fi
 version=`$PROTOC --version`
 
 cd ${protoDir}
+echo "cd ${protoDir}"
 
-for proto_file in */*.proto; do
-    ## 替换路径
-    pb_file=${proto_file/protos/pkg}
-    pb_file=${pb_file/.proto/.pb.go}
-    echo $proto_file
-    echo $pb_file
-    if [ ! -e $pb_file ] || [ $proto_file -nt $pb_file ]; then
+for dirName in */;do
+  echo "${dirName}"
+  for proto_file in ${dirName}*.proto; do
+      ## 替换路径
+      pb_file=${proto_file/.proto/.pb.go}
 
-        echo "Compiling $proto_file -> $pb_file ..."
-        # 输出到 ../${pb_package}
-        # 一般都会使用source_relative,否则，就会按照option go_package里面的那个路径生成
-        # -I 表示包含全部
-        $PROTOC  -I $proto_file --plugin=protoc-gen-go=../../bin/protoc-gen-go.exe --go_out=../${pb_package} --go_opt=paths=source_relative
-    fi
+      if [ ! -e $pb_file ] || [ $proto_file -nt $pb_file ]; then
+
+          echo "Compiling $proto_file -> $pb_file ..."
+          # 输出到 ../${pb_package}
+          # 一般都会使用source_relative,否则，就会按照option go_package里面的那个路径生成
+          # -I 表示包含全部
+          $PROTOC --proto_path=${dirName} --proto_path=. --go_out=../${pb_package}/${dirName} --go_opt=paths=source_relative $proto_file --plugin=protoc-gen-go=../../bin/protoc-gen-go.exe
+      fi
+  done
 done
