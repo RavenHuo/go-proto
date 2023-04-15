5ecccbfac6f9b035638b3faa3236a9150dd032b6
huoruiwen <ruiwen.rw@nonolive.com>
2023-04-15 22:20:08 +0800

    pb

--- stock-proto/make.sh
+++ stock-proto/make.sh
@@ -10,7 +10,7 @@ if (which protoc.exe) then
 fi
 
 
-if [ -e ${pb_package} ]
+if [ -d ${pb_package} ]
 then
 echo "${pb_package} 已存在"
 else
@@ -31,8 +31,13 @@ for dirName in */;do
       pb_file=${proto_file/.proto/.pb.go}
 
       if [ ! -e $pb_file ] || [ $proto_file -nt $pb_file ]; then
+          if [ ! -d ../${pb_package}/${dirName} ];then
+            mkdir ../${pb_package}/${dirName}
+            echo "../${pb_package}/${dirName}目录不存在，创建文件夹"
+          fi
 
           echo "Compiling $proto_file -> $pb_file ..."
+
           # 输出到 ../${pb_package}
           # 一般都会使用source_relative,否则，就会按照option go_package里面的那个路径生成
           #  --proto_path 表示引用外部的proto
